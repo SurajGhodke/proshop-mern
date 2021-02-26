a4c5dfd08fa7951dfcb8d20a25ee7092c6031257
Suraj Ghodke <surajghodke0102@gmail.com>
2021-02-26 20:19:23 +0530

    implement react router

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,6 +1,6 @@
 import React from "react";
+import propTypes from "prop-types";
 
-import PropTypes from "prop-types";
 const Rating = ({ value, text, color }) => {
   return (
     <div className="rating">
@@ -73,10 +73,10 @@ Rating.defaultProps = {
   color: "#f8e825",
 };
 
-Rating.PropTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string,
+Rating.propTypes = {
+  value: propTypes.number.isRequired,
+  text: propTypes.string.isRequired,
+  color: propTypes.string,
 };
 
 export default Rating;
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductScreen = () => {
+  return <div>products</div>;
+};
+
+export default ProductScreen;
